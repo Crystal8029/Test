1924667e5448038792f468dad1426991646cdcc5
Crystal8029 <86389201+Crystal8029@users.noreply.github.com>
2021-10-27 09:09:19 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <h2>CodeSandbox</h2>
+      <p> badfshfoisSugarsdfdfsAKAdfsiTangunsdfrepTang </p>
+    </div>
+  );
+}
